60280a65c76dc205d9928e4ea591c4db6c1da4e4
Patrick <patrick.mcguigan5@gmail.com>
2022-08-09 06:34:26 -0600

    dropdown menu works, need to work on display animation and menu styling, then portfolio page.

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -1,9 +1,15 @@
 const dropdownBtn = document.querySelector('.dropdown-btn');
 const dropdownMenu = document.querySelector('.dropdown-toggle-container');
+const closeBtn = document.querySelector('.close-menu-btn');
 
 dropdownBtn.addEventListener('click', e => {
     dropdownMenu.style.display = 'flex';
     dropdownMenu.style.flexDirection = 'column';
     dropdownMenu.style.justifyContent = 'center';
     dropdownMenu.style.alignItems = 'center';  
+});
+
+closeBtn.addEventListener('click', e => {
+    dropdownMenu.style.display = 'none';
+    dropdownMenu.style.animation = 'dropdown .5s';
 });
