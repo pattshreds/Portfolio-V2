131315bfde818e86ef1e464ca3d42845993ac31f
Patrick <patrick.mcguigan5@gmail.com>
2022-11-06 17:08:39 -0700

    changed portfolio.html to index.html for deployment with netlify

--- components/menu.js
+++ components/menu.js
@@ -6,7 +6,7 @@ class Menu extends HTMLElement {
         this.innerHTML = `
         <div class="dropdown-toggle-container">
             <p class='close-menu-btn'>x</p>
-            <a class='dropdown-item' href="portfolio.html">Home</a>
+            <a class='dropdown-item' href="index.html">Home</a>
             <a class='dropdown-item' href="about.html">About</a>
             <a class='dropdown-item' href="projects.html">Projects</a>
             <a class='dropdown-item' href="contact.html">Contact</a>
