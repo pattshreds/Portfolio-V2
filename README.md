b13f4117ba6eec33fabb3deb029531124f5ee30a
Patrick Mcguigan <pattshreds@patricks-mbp.lan>
2024-04-24 18:00:32 -0600

    Restyled full website. Added new root values in Globalstyles for primary colors. Added functionality in nav.js to hide the link for the current path; ex. home link is hidden on home page. Cleaned up keyframe animations in globalStyles. Formatted bootsrap contact form layout, and overrode bootstrap default root values that were interfering with my navStyles on the contact page. Made styling improvements throughout.

--- README.md
+++ README.md
@@ -3,17 +3,14 @@
 <p>
   This time around I'm using HTML, CSS, and Javascript. My new portfolio will serve as a portfolio, contact page, and in the future a blog.
 </p>
-<br /> <br />
+<br />
 This website is hosted through Netlify. View <a href="https://pmsoftware.netlify.app/" alt="My Portfolio">here</a>
-
-<hr>
-
+<hr />
 <p>
   No installation necessary to run locally. Clone and open any html file in your browser if you wish!
 </p>
-
+<hr />
 ## To-do List
-
 <ul>
   <li><s> Eliminate dropdown menu icon. </s></li>
   <li><s> Build Experience and Contact page. </s></li>
--- components/nav.js
+++ components/nav.js
@@ -6,19 +6,22 @@ class Nav extends HTMLElement {
         this.innerHTML = `
         <nav id="navbar">
             <p id="logo"> Placeholder </p>
-            <button id="resume-button">
+            <button class="nav-button resume">
                 <a href="images/PM_SE_Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
             </button>
-            <button class="nav-button home-button">
+            <button class="nav-button github">
+                <a href="https://github.com/pattshreds" target="_blank" rel="noopener noreferrer">GitHub</a>
+            </button>
+            <button class="nav-button home">
                 <a href="./index.html"> Home </a>
             </button>
-            <button class="nav-button about-button">
+            <button class="nav-button about">
                 <a href="./about.html"> About </a>
             </button>
-            <button class="nav-button projects-button">
+            <button class="nav-button projects">
                 <a href="./projects.html"> Projects </a>
             </button>
-            <button class="nav-button contact-button">
+            <button class="nav-button contact">
                 <a href="./contact.html"> Contact </a>
             </button>
         </nav>
@@ -28,6 +31,24 @@ class Nav extends HTMLElement {
 
 customElements.define('nav-component', Nav);
 
+setTimeout(() => {
+    const pathname = window.location.pathname;
+    const homeButton = document.querySelector('.home');
+    const aboutButton = document.querySelector('.about');
+    const projectsButton = document.querySelector('.projects');
+    const contactButton = document.querySelector('.contact');
+    if (pathname === '/index.html') {
+        homeButton.style.display = 'none';
+    } else if (pathname === '/about.html') {
+        aboutButton.style.display = 'none';
+    } else if (pathname === '/projects.html') {
+        projectsButton.style.display = 'none';
+    } else if (pathname === '/contact.html') {
+        contactButton.style.display = 'none';
+    }
+});
+
+// May use this again, may not
 {
     /* <button id='email' onclick='copyText()'>
                     <p id='email-text'>patrick.mcguigan5@gmail.com</p> 
