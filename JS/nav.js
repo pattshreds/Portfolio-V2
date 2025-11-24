1c95177de8fa990daabe9bd10f2e37f7d0ef4a8f
Patrick Mcguigan <patrick.mcguigan5@gmail.com>
2025-11-24 14:52:26 -0700

    Decided to display the logo on mobile screens so I removed the display none from logo and styledaccordingly.

--- JS/nav.js
+++ JS/nav.js
@@ -6,9 +6,6 @@ setTimeout(() => {
     const contactButton = document.querySelector('.contact');
     const viewportWidth = window.innerWidth;
     const logo = document.getElementById('logo');
-    if (viewportWidth < 500) {
-        logo.style.display = 'none';
-    }
     if (pathname === '/') {
         homeButton.style.display = 'none';
     } else if (pathname === '/index.html') {
